332952cd272e00d04450a11afcfcdfb227dac0d7
Mark57449 <wendel.souza2@etec.sp.gov.br>
2018-09-20 14:33:08 -0300

    Alterações feitas para adaptação do Tab-view.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppRegistry, Image, ScrollView, StyleSheet, Text, TextInput, Button, Alert, TouchableOpacity, Dimensions, View} from 'react-native';
+import { AppRegistry, Image, ScrollView, StyleSheet, Text, TextInput, Button, Alert, TouchableOpacity, Dimensions, View, Platform} from 'react-native';
 import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 /*
 var React = require('react');
@@ -15,44 +15,7 @@ var Alert = require('react-native').Alert;
 var
 */
 
-const Inicio = () => (
-  
-      <ScrollView style={MainStyle.conteudo} showsVerticalScrollIndicator={false}>
-        <Image source={require('./src/img/Geeko!!.png')} style={{width: 320, height: 100, marginVertical: 13, resizeMode: 'contain'}} />
-        <Text style={MainStyle.InfoText}>Seja bem-vindo, o nosso aplicativo tem a proposta de sugerir um meio de tornar a sua dieta mais fácil com a simples ideia de um diário alimentar, e um filtro de alimentos.</Text>
-
-        <TouchableOpacity style={[MainStyle.buttonMain, MainStyle.BtnRoxo]} accessibilityLabel="Learn more about this purple button">
-          <Text style={MainStyle.buttonText}>FAÇA O CADASTRO DO SEU PERFIL</Text>
-        </TouchableOpacity>  
-        <TouchableOpacity style={[MainStyle.buttonMain, MainStyle.BtnLaranja]} accessibilityLabel="Learn more about this purple button">
-          <Text style={MainStyle.buttonText}>CRIE A SUA PRIMEIRA ROTINA</Text>
-        </TouchableOpacity>  
-        <TouchableOpacity style={[MainStyle.buttonMain, MainStyle.BtnAzul]} accessibilityLabel="Learn more about this purple button">
-          <Text style={MainStyle.buttonText}>ENCONTRE SOBRE OS ALIMENTOS</Text>
-        </TouchableOpacity>  
-        <TouchableOpacity style={[MainStyle.buttonMain, MainStyle.BtnVerde]} accessibilityLabel="Learn more about this purple button">
-          <Text style={MainStyle.buttonText}>NOS DE O SEU FEEDBACK :)</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={MainStyle.center} accessibilityLabel="Learn more about this purple button">
-          <Image source={require('./src/img/arrow-down.png')} style={{width: 40, height: 40, resizeMode: 'contain'}}/>
-        </TouchableOpacity>
-
-        <View style={{ backgroundColor: '#00F'}}>
-          <Text style={MainStyle.InfoText}>Seja bem-vindo, o nosso aplicativo tem a proposta de sugerir um meio de tornar a sua dieta mais fácil com a simples ideia de um diário alimentar, e um filtro de alimentos.</Text>
-          <Text style={MainStyle.InfoText}>Seja bem-vindo, o nosso aplicativo tem a proposta de sugerir um meio de tornar a sua dieta mais fácil com a simples ideia de um diário alimentar, e um filtro de alimentos.</Text>
-          <Text style={MainStyle.InfoText}>Seja bem-vindo, o nosso aplicativo tem a proposta de sugerir um meio de tornar a sua dieta mais fácil com a simples ideia de um diário alimentar, e um filtro de alimentos.</Text>
-          <Text style={MainStyle.InfoText}>Seja bem-vindo, o nosso aplicativo tem a proposta de sugerir um meio de tornar a sua dieta mais fácil com a simples ideia de um diário alimentar, e um filtro de alimentos.</Text>
-        </View>
-      </ScrollView> 
-);
-const Rotinas = () => (
-  <ScrollView style={{ backgroundColor: '#673ab7' }} />
-);
-
-const Alimentos = () => (
-  <ScrollView style={{ backgroundColor: '#673ab7' }} />
-);
+let scrollYPos = 0;
 
 export default class App extends React.Component {
   constructor(props) {
@@ -61,20 +24,28 @@ export default class App extends React.Component {
       screenHeight: Dimensions.get('window').height,      
       screenWidth: Dimensions.get('window').width,
       index: 0,
-    routes: [
-      { key: '0', title: 'Inicio' },
-      { key: '1', title: 'Rotinas' },
-      { key: '2', title: 'Alimentos' },
-    ],
+      routes: [
+        { key: '0', title: 'Inicio' },
+        { key: '1', title: 'Rotinas' },
+        { key: '2', title: 'Alimentos' },
+      ],
     };
   }
 
-
+  scrollToB = () => {
+    scrollYPos = this.state.screenHeight * 1;
+    this.scroller.scrollTo({x: 0, y: scrollYPos});
+  };
+  scrollToC = () => {
+    scrollYPos = this.size.screenHeight * 2;
+    this.scroller.scrollTo({x: 0, y: scrollYPos});
+  };
+  scrollToTop = () => {
+    this.scroller.scrollTo({x: 0, y: 0});
+  };
   
 
   render() {
-
-  //const App = () => {
     return (
       
         <TabView
@@ -92,10 +63,61 @@ export default class App extends React.Component {
  }
 };
 
+function Rotinas(){
+  scrollToEnd({animated: true});
+};
+
+Inicio = () => (
+  
+      <ScrollView style={MainStyle.conteudo} ref={(scroller) => {this.scroller = scroller}}>
+        <View>
+          <Image source={require('./src/img/Geeko!!.png')} style={{width: 320, height: 100, marginVertical: 13, resizeMode: 'contain'}} />
+          <Text style={MainStyle.InfoText}>Seja bem-vindo, o nosso aplicativo tem a proposta de sugerir um meio de tornar a sua dieta mais fácil com a simples ideia de um diário alimentar, e um filtro de alimentos.</Text>
+
+          <TouchableOpacity style={[MainStyle.buttonMain, MainStyle.BtnRoxo]} accessibilityLabel="Learn more about this purple button">
+            <Text style={MainStyle.buttonText}>FAÇA O CADASTRO DO SEU PERFIL</Text>
+          </TouchableOpacity>  
+          <TouchableOpacity style={[MainStyle.buttonMain, MainStyle.BtnLaranja]} accessibilityLabel="Learn more about this purple button">
+            <Text style={MainStyle.buttonText}>CRIE A SUA PRIMEIRA ROTINA</Text>
+          </TouchableOpacity>  
+          <TouchableOpacity style={[MainStyle.buttonMain, MainStyle.BtnAzul]} accessibilityLabel="Learn more about this purple button">
+            <Text style={MainStyle.buttonText}>ENCONTRE SOBRE OS ALIMENTOS</Text>
+          </TouchableOpacity>  
+          <TouchableOpacity style={[MainStyle.buttonMain, MainStyle.BtnVerde]} accessibilityLabel="Learn more about this purple button">
+            <Text style={MainStyle.buttonText}>NOS DE O SEU FEEDBACK :)</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={MainStyle.center} onPress={this.scrollToB} accessibilityLabel="Learn more about this purple button">
+            <Image source={require('./src/img/arrow-down.png')} style={{width: 40, height: 40, resizeMode: 'contain'}}/>
+          </TouchableOpacity>
+        </View>
+
+        <View style={MainStyle.screen}>
+          <Text style={MainStyle.InfoText}>Seja bem-vindo, o nosso aplicativo tem a proposta de sugerir um meio de tornar a sua dieta mais fácil com a simples ideia de um diário alimentar, e um filtro de alimentos.</Text>
+          <Text style={MainStyle.InfoText}>Seja bem-vindo, o nosso aplicativo tem a proposta de sugerir um meio de tornar a sua dieta mais fácil com a simples ideia de um diário alimentar, e um filtro de alimentos.</Text>
+          <Text style={MainStyle.InfoText}>Seja bem-vindo, o nosso aplicativo tem a proposta de sugerir um meio de tornar a sua dieta mais fácil com a simples ideia de um diário alimentar, e um filtro de alimentos.</Text>
+          <Text style={MainStyle.InfoText}>Seja bem-vindo, o nosso aplicativo tem a proposta de sugerir um meio de tornar a sua dieta mais fácil com a simples ideia de um diário alimentar, e um filtro de alimentos.</Text>
+          <Text style={MainStyle.InfoText}>Seja bem-vindo, o nosso aplicativo tem a proposta de sugerir um meio de tornar a sua dieta mais fácil com a simples ideia de um diário alimentar, e um filtro de alimentos.</Text>
+        </View>
+      </ScrollView> 
+);
+const Rotinas = () => (
+  <ScrollView style={{ backgroundColor: '#673ab7' }} />
+);
+
+const Alimentos = () => (
+  <ScrollView style={{ backgroundColor: '#673ab7' }} />
+);
+
+
 const MainStyle = StyleSheet.create({
   conteudo: {
     paddingTop: 5,
     marginHorizontal: 30,
+    flex: 1,
+  },
+  screen: {
+    height: Dimensions.get('window').height,
   },
   buttonMain: {
     padding: 12,
