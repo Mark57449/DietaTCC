f8c42a1af45564212a6eaf71e966c6a42a11c393
Mark57449 <wendel.souza2@etec.sp.gov.br>
2018-10-14 16:08:38 -0300

    Adicionado o Axios para requisição de JSON externo, Estilo alterado, adicionado Componente de Alimentos com FlatList.

--- src/components/Alimentos.js
+++ src/components/Alimentos.js
@@ -1,27 +1,58 @@
 import React from 'react';
-import { Image, ScrollView, StyleSheet, Text, TextInput, Button, Alert, TouchableOpacity, View } from 'react-native';
+import { Image, ScrollView, FlatList, StyleSheet, Text, TextInput, Button, Alert, TouchableOpacity, View } from 'react-native';
+import axios from 'axios';
 
-import PesquisaDeAlimentos from './adds/PesquisaDeAlimentos.js';
+import Item from './adds/Item.js';
+import data from '../data.js';
+
+	let numberGrid = 1;
+
 
 class Alimentos extends React.Component {
 	constructor(props){
     	super(props);
+
+    	this.state = { 
+    		text: 'Pesquise aqui sobre o alimento que quiser.',
+    		listaItens: [],
+    		data: data
+    	}
+	}
+
+	// componentWillMount(){
+	// 	axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+	// 	.then(response => { this.setState({ listaItens: response.data }); })
+	// 	.catch(() => { console.log("Erro ao recuperar os dados."); });
+	// }
+
+	renderItem = ({item}) => {
+	   return <Item style={EstilosRotinas.item} nome={item.nome} kcal={item.kcal} estado={item.estado} status="PlusUltra"/>;
 	}
 
 	render(){
 		return (
-			<View>
-				<View>
-					<Text>Sem problemas!</Text>
-					<Text>Não recomendado!</Text>
+			<View style={{ paddingVertical: 20, paddingHorizontal: 15}}>
+				<View style={EstilosRotinas.ladoALado}>
+					<Image source={require('../img/squareOk.png')} style={{ width: 24, height: 24, paddingTop: 2 }} /> 
+					<Text style={[EstilosRotinas.legenda, { color: '#65B545' }]}> Sem problemas! </Text>
+					<Image source={require('../img/squareNo.png')} style={{ width: 24, height: 24, paddingTop: 2 }} />
+					<Text style={[EstilosRotinas.legenda, { color: '#F95F62'}]}> Não recomendado! </Text>
 				</View>
 				<View>
-					<Text>Sem problemas!</Text>
-					<Text>Não recomendado!</Text>
+					<TextInput
+				        style={EstilosRotinas.InputPesquisa}
+				        onChangeText={(text) => this.setState({text})}
+				        placeholder={this.state.text}
+				        autoCapitalize="sentences"
+				      />
 				</View>
 				<ScrollView>
-					<Text>Sem problemas!</Text>
-					<Text>Não recomendado!</Text>
+					<FlatList  
+			          keyExtractor={(_, index) => index.toString()} 
+			          numColumns={numberGrid} data={this.state.data} 
+			          renderItem={this.renderItem} />
+
+					
 				</ScrollView>
 			</View>
 		);
@@ -29,9 +60,30 @@ class Alimentos extends React.Component {
 };
 
 const EstilosRotinas = StyleSheet.create ({
-	aaaa: {
-		color: '#000',
-	}
+	Info: {
+		color: '#333',
+		textAlign: 'center',
+		fontSize: 16,
+	},
+	ladoALado: {
+	    flexDirection: 'row',
+	    justifyContent: 'space-between',
+  	},
+  	legenda: {
+  		justifyContent: 'center',
+  		fontWeight: 'bold',
+  	},
+  	InputPesquisa: {
+  		height: 60,
+  		borderColor: '#00000030',
+  		borderWidth: 0.5,
+  		borderRadius: 5,
+  		marginVertical: 10,
+  		paddingHorizontal: 13,
+  	},
+  	item: {
+		flexDirection: 'row',
+	},
 });
 
 export default Alimentos;
