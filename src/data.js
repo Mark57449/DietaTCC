281197427b543178bd2029bf41b2a22c4ef8d6d0
Mark57449 <wendel.souza2@etec.sp.gov.br>
2018-10-14 16:02:34 -0300

    Exemplo de OBJETO LITERAL para exibição de dados de alimentos.

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export default [
+	{
+		id: 1,
+		nome: "Banana",
+		kcal: "230",
+		estado: "Cozido(a)"
+	},
+	{
+		id: 2,
+		nome: "Cogumelo in Natura",
+		kcal: "230",
+		estado: "Cozido(a)"
+	},
+	{
+		id: 3,
+		nome: "Laranja",
+		kcal: "230",
+		estado: "Cozido(a)"
+	},
+	{
+		id: 4,
+		nome: "Beterraba",
+		kcal: "230",
+		estado: "Cozido(a)"
+	},
+	{
+		id: 5,
+		nome: "Beringela",
+		kcal: "230",
+		estado: "Cozido(a)"
+	},
+	{
+		id: 6,
+		nome: "Banana",
+		kcal: "230",
+		estado: "Cozido(a)"
+	},
+	{
+		id: 7,
+		nome: "Cogumelo in Natura",
+		kcal: "230",
+		estado: "Cozido(a)"
+	},
+	{
+		id: 8,
+		nome: "Laranja",
+		kcal: "230",
+		estado: "Cozido(a)"
+	},
+	{
+		id: 9,
+		nome: "Beterraba",
+		kcal: "230",
+		estado: "Cozido(a)"
+	},
+	{
+		id: 10,
+		nome: "Beringela",
+		kcal: "230",
+		estado: "Cozido(a)"
+	}
+]
